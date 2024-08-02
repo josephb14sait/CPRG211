4ff5613456dfe451330553a2b1b4254867a2f689
josephb14sait <152675414+josephb14sait@users.noreply.github.com>
2024-08-01 22:26:36 -0600

    Add files via upload

---
+++ Node.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Assignment3
+{
+    [DataContract]
+    public class Node
+    {
+        [DataMember]
+        public User Value { get; set; }
+        [DataMember]
+        public Node Next { get; set; }
+
+        public Node() { }
+
+        public Node(User value)
+        {
+            Value = value;
+            Next = null;
+
+        }
+
+    }
+}
+
